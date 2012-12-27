664886727ae6e2c86a820f62a6e12af6a2d78bd9
cmb69 <cmbecker69@gmx.de>
2012-12-27 21:31:57 +0000

    added slovak localization (contributed by Tata)

---
+++ languages/sk.php
@@ -0,0 +1,37 @@
+<?php
+
+$plugin_tx['socialshareprivacy']['general_info_link']="http://www.heise.de/ct/artikel/2-Klicks-fuer-mehr-Datenschutz-1333879.html";
+$plugin_tx['socialshareprivacy']['general_help']="Ak aktivujete toto tlačidlo kliknutím, informácie budú odoslané na Facebook, Twitter, resp. Google v USA a možno tam budú aj uchované. Pre viac detailov kliknite na <em>i</em>.";
+$plugin_tx['socialshareprivacy']['general_settings_perma']="Aktivovať permanentne a povoliť prenos údajov:";
+$plugin_tx['socialshareprivacy']['general_country_code']="US";
+$plugin_tx['socialshareprivacy']['facebook_info']="2 kliknutia pre viac súkromia: tlačidlo bude aktívne až po kliknutí a budete môcť poslať doporučenie na Facebook. Akonáhle aktivujete tlačidlo, údaje budú odoslané tretej strane &ndash; pozrite na <em>i</em>.";
+$plugin_tx['socialshareprivacy']['facebook_off']="nepripojené na Facebook";
+$plugin_tx['socialshareprivacy']['facebook_on']="pripojené na Facebook";
+$plugin_tx['socialshareprivacy']['facebook_display_name']="Facebook";
+$plugin_tx['socialshareprivacy']['twitter_info']="2 kliknutia pre viac súkromia: tlačidlo bude aktívne až po kliknutí a budete môcť poslať doporučenie na Twitter. Akonáhle aktivujete tlačidlo, údaje budú odoslané tretej strane &ndash; pozrite na <em>i</em>.";
+$plugin_tx['socialshareprivacy']['twitter_off']="nepripojené na Twitter";
+$plugin_tx['socialshareprivacy']['twitter_on']="pripojené to Twitter";
+$plugin_tx['socialshareprivacy']['twitter_display_name']="Twitter";
+$plugin_tx['socialshareprivacy']['gplus_info']="2 kliknutia pre viac súkromia: tlačidlo bude aktívne až po kliknutí a budete môcť poslať doporučenie na Google+. Akonáhle aktivujete tlačidlo, údaje budú odoslané tretej strane &ndash; pozrite na <em>i</em>.";
+$plugin_tx['socialshareprivacy']['gplus_off']="nepripojené na Google+";
+$plugin_tx['socialshareprivacy']['gplus_on']="pripojené na Google+";
+$plugin_tx['socialshareprivacy']['gplus_display_name']="Google+";
+$plugin_tx['socialshareprivacy']['syscheck_title']="Kontrola systému";
+$plugin_tx['socialshareprivacy']['syscheck_phpversion']="PHP verzia &ge; %s";
+$plugin_tx['socialshareprivacy']['syscheck_extension']="Rozšírenie '%s' načítané";
+$plugin_tx['socialshareprivacy']['syscheck_encoding']="Kódovanie 'UTF-8' nastavené";
+$plugin_tx['socialshareprivacy']['syscheck_magic_quotes']="Magic quotes runtime off";
+$plugin_tx['socialshareprivacy']['syscheck_writable']="Adresár '%s' umožňuje zápis";
+$plugin_tx['socialshareprivacy']['cf_cookie_expires']="Čas úplatnosti cookies (v dňoch).";
+$plugin_tx['socialshareprivacy']['cf_facebook_status']="Má užívateľ k dispozícii Facebook?: \"on\" alebo \"off\".";
+$plugin_tx['socialshareprivacy']['cf_facebook_perma_option']="Môže si užívateľ aktivovať Facebook permanentne? (cookies musia byť povolené): \"on\" alebo \"off\".";
+$plugin_tx['socialshareprivacy']['cf_facebook_referrer_track']="Bude pridané k URL; môže sa využiť na sledovanie referujúcich.";
+$plugin_tx['socialshareprivacy']['cf_facebook_action']="Nadpis tlačidla: \"recommend\" (Doporuč) alebo \"like\" (Páči sa).";
+$plugin_tx['socialshareprivacy']['cf_twitter_status']="Má užívateľ k dispozícii Twitter?: \"on\" alebo \"off\".";
+$plugin_tx['socialshareprivacy']['cf_twitter_perma_option']="Môže si užívateľ aktivovať Twitter permanentne? (cookies musia byť povolené): \"on\" alebo \"off\".";
+$plugin_tx['socialshareprivacy']['cf_twitter_referrer_track']="Bude pridané k URL; môže sa využiť na sledovanie referujúcich.";
+$plugin_tx['socialshareprivacy']['cf_gplus_status']="Má užívateľ k dispozícii Google+?: \"on\" alebo \"off\".";
+$plugin_tx['socialshareprivacy']['cf_gplus_perma_option']="Môže si užívateľ aktivovať Google+ permanentne? (cookies musia byť povolené): \"on\" alebo \"off\".";
+$plugin_tx['socialshareprivacy']['cf_gplus_referrer_track']="Bude pridané k URL; môže sa využiť na sledovanie referujúcich.";
+
+?>
