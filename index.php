ec0c7021da1eb326be35ac086b9ded3c691021bc
Christoph M. Becker <cmbecker69@gmx.de>
2015-03-07 14:16:15 +0100

    fixed doc block inconsistencies

--- index.php
+++ index.php
@@ -6,10 +6,10 @@
  * PHP versions 4 and 5
  *
  * @category  CMSimple_XH
- * @package   Socialshareprivacy_XH
+ * @package   Socialshareprivacy
  * @author    Christoph M. Becker <cmbecker69@gmx.de>
- * @copyright 2012-2015 Christoph M. Becker
- * @license   GNU GPL v3
+ * @copyright 2012-2015 Christoph M. Becker <http://3-magi.net>
+ * @license   http://www.gnu.org/licenses/gpl-3.0.en.html GNU GPLv3
  * @link      http://3-magi.net/?CMSimple_XH/Socialshareprivacy_XH
  */
 
