ef5410ba7cc13e39bd0f920451b6430cfadcf517
Christoph M. Becker <cmbecker69@gmx.de>
2025-06-03 19:21:31 +0200

    Remove superfluous license etc. from plugin info screen

--- classes/Controller.php
+++ classes/Controller.php
@@ -159,43 +159,12 @@ class Controller
         $o .= print_plugin_admin('off');
         switch ($admin) {
             case '':
-                $o .= self::renderVersion() . self::renderSystemCheck();
+                $o .= "<h1>Socialshareprivacy " . XH_hsc(SOCIALSHAREPRIVACY_VERSION) . "</h1>\n"
+                    . self::renderSystemCheck();
                 break;
             default:
                 $o .= plugin_admin_common($action, $admin, 'socialshareprivacy'); // @phpstan-ignore-line
         }
-    }
-
-    private static function renderVersion(): string
-    {
-        global $pth, $plugin_tx;
-
-        $image = tag(
-            'img class="socialshareprivacy_logo" src="' . $pth['folder']['plugins']
-            . 'socialshareprivacy/socialshareprivacy.png" alt="'
-            . $plugin_tx['socialshareprivacy']['alt_logo'] . '"'
-        );
-        $version = SOCIALSHAREPRIVACY_VERSION;
-        return <<<EOT
-<h1>Socialshareprivacy &ndash; {$plugin_tx['socialshareprivacy']['menu_info']}</h1>
-$image
-<p>Version: $version</p>
-<p>Copyright &copy; 2012-2015 <a href="http://3-magi.net/">Christoph M.
-Becker</a></p>
-<p>Socialshareprivacy_XH is powered by <a
-href="http://www.heise.de/extras/socialshareprivacy/">socialshareprivacy</a>.
-<p class="socialshareprivacy_license">This program is free software: you can
-redistribute it and/or modify it under the terms of the GNU General Public
-License as published by the Free Software Foundation, either version 3 of the
-License, or (at your option) any later version.</p>
-<p class="socialshareprivacy_license">This program is distributed in the hope
-that it will be useful, but <em>without any warranty</em>; without even the
-implied warranty of <em>merchantability</em> or <em>fitness for a particular
-purpose</em>. See the GNU General Public License for more details.</p>
-<p class="socialshareprivacy_license">You should have received a copy of the GNU
-General Public License along with this program. If not, see <a
-href="http://www.gnu.org/licenses/">http://www.gnu.org/licenses/</a>.</p>
-EOT;
     }
 
     private static function renderSystemCheck(): string
