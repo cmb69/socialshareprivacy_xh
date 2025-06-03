1699b01111ff3b01a1907c6ab6a3115e2a0f013e
Christoph M. Becker <cmbecker69@gmx.de>
2025-06-04 00:45:04 +0200

    Proper HTML5 structure for plugin info

--- views/info.php
+++ views/info.php
@@ -11,8 +11,12 @@ if (!defined("CMSIMPLE_XH_VERSION")) {http_response_code(403); exit;}
  */
 ?>
 
-<h1>Socialshareprivacy <?=$this->esc($version)?></h1>
-<h4><?=$this->text("syscheck_title")?></h4>
+<article class="socialshareprivacy_pluginfo">
+  <h1>Socialshareprivacy <?=$this->esc($version)?></h1>
+  <section class="socialshareprivacy_syscheck">
+    <h2><?=$this->text("syscheck_title")?></h2>
 <?foreach ($checks as $check):?>
-<?=$this->raw($check)?>
+    <?=$this->raw($check)?>
 <?endforeach?>
+  </section>
+</article>
