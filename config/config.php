9aa890c7f3f59d2c96f4bfc545ef619f9710f6be
Christoph M. Becker <cmbecker69@gmx.de>
2025-06-03 19:16:45 +0200

    Apply coding standards to config and language files

--- config/config.php
+++ config/config.php
@@ -1,23 +1,21 @@
 <?php
 
-$plugin_cf['socialshareprivacy']['template_call']="0";
-$plugin_cf['socialshareprivacy']['url']="";
-$plugin_cf['socialshareprivacy']['cookie_expires']="365";
-$plugin_cf['socialshareprivacy']['facebook_status']="on";
-$plugin_cf['socialshareprivacy']['facebook_perma_option']="on";
-$plugin_cf['socialshareprivacy']['facebook_referrer_track']="";
-$plugin_cf['socialshareprivacy']['facebook_action']="like";
-$plugin_cf['socialshareprivacy']['twitter_status']="on";
-$plugin_cf['socialshareprivacy']['twitter_perma_option']="on";
-$plugin_cf['socialshareprivacy']['twitter_referrer_track']="";
-$plugin_cf['socialshareprivacy']['gplus_status']="on";
-$plugin_cf['socialshareprivacy']['gplus_perma_option']="on";
-$plugin_cf['socialshareprivacy']['gplus_referrer_track']="";
-$plugin_cf['socialshareprivacy']['xing_status']="on";
-$plugin_cf['socialshareprivacy']['xing_perma_option']="on";
-$plugin_cf['socialshareprivacy']['xing_referrer_track']="";
-$plugin_cf['socialshareprivacy']['linkedin_status']="on";
-$plugin_cf['socialshareprivacy']['linkedin_perma_option']="on";
-$plugin_cf['socialshareprivacy']['linkedin_referrer_track']="";
-
-?>
+$plugin_cf['socialshareprivacy']['template_call'] = "0";
+$plugin_cf['socialshareprivacy']['url'] = "";
+$plugin_cf['socialshareprivacy']['cookie_expires'] = "365";
+$plugin_cf['socialshareprivacy']['facebook_status'] = "on";
+$plugin_cf['socialshareprivacy']['facebook_perma_option'] = "on";
+$plugin_cf['socialshareprivacy']['facebook_referrer_track'] = "";
+$plugin_cf['socialshareprivacy']['facebook_action'] = "like";
+$plugin_cf['socialshareprivacy']['twitter_status'] = "on";
+$plugin_cf['socialshareprivacy']['twitter_perma_option'] = "on";
+$plugin_cf['socialshareprivacy']['twitter_referrer_track'] = "";
+$plugin_cf['socialshareprivacy']['gplus_status'] = "on";
+$plugin_cf['socialshareprivacy']['gplus_perma_option'] = "on";
+$plugin_cf['socialshareprivacy']['gplus_referrer_track'] = "";
+$plugin_cf['socialshareprivacy']['xing_status'] = "on";
+$plugin_cf['socialshareprivacy']['xing_perma_option'] = "on";
+$plugin_cf['socialshareprivacy']['xing_referrer_track'] = "";
+$plugin_cf['socialshareprivacy']['linkedin_status'] = "on";
+$plugin_cf['socialshareprivacy']['linkedin_perma_option'] = "on";
+$plugin_cf['socialshareprivacy']['linkedin_referrer_track'] = "";
--- config/metaconfig.php
+++ config/metaconfig.php
@@ -1,17 +1,14 @@
 <?php
 
-$plugin_mcf['socialshareprivacy']['template_call']="bool";
-$plugin_mcf['socialshareprivacy']['facebook_status']="enum:on,off";
-$plugin_mcf['socialshareprivacy']['facebook_perma_option']="enum:on,off";
-$plugin_mcf['socialshareprivacy']['facebook_action']="enum:recommend,like";
-$plugin_mcf['socialshareprivacy']['twitter_status']="enum:on,off";
-$plugin_mcf['socialshareprivacy']['twitter_perma_option']="enum:on,off";
-$plugin_mcf['socialshareprivacy']['gplus_status']="enum:on,off";
-$plugin_mcf['socialshareprivacy']['gplus_perma_option']="enum:on,off";
-$plugin_mcf['socialshareprivacy']['xing_status']="enum:on,off";
-$plugin_mcf['socialshareprivacy']['xing_perma_option']="enum:on,off";
-$plugin_mcf['socialshareprivacy']['linkedin_status']="enum:on,off";
-$plugin_mcf['socialshareprivacy']['linkedin_perma_option']="enum:on,off";
-
-
-?>
+$plugin_mcf['socialshareprivacy']['template_call'] = "bool";
+$plugin_mcf['socialshareprivacy']['facebook_status'] = "enum:on,off";
+$plugin_mcf['socialshareprivacy']['facebook_perma_option'] = "enum:on,off";
+$plugin_mcf['socialshareprivacy']['facebook_action'] = "enum:recommend,like";
+$plugin_mcf['socialshareprivacy']['twitter_status'] = "enum:on,off";
+$plugin_mcf['socialshareprivacy']['twitter_perma_option'] = "enum:on,off";
+$plugin_mcf['socialshareprivacy']['gplus_status'] = "enum:on,off";
+$plugin_mcf['socialshareprivacy']['gplus_perma_option'] = "enum:on,off";
+$plugin_mcf['socialshareprivacy']['xing_status'] = "enum:on,off";
+$plugin_mcf['socialshareprivacy']['xing_perma_option'] = "enum:on,off";
+$plugin_mcf['socialshareprivacy']['linkedin_status'] = "enum:on,off";
+$plugin_mcf['socialshareprivacy']['linkedin_perma_option'] = "enum:on,off";
